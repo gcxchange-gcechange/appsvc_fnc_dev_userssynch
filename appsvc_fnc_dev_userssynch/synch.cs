a4932dd6a7d44b50b69e60668a9964e2b2d7513e
Stéphanie Lefebvre <STLEFEBV@tbs-sct.gc.ca>
2023-01-25 08:58:49 -0500

    Cobert to upper case alias

--- appsvc_fnc_dev_userssynch/synch.cs
+++ appsvc_fnc_dev_userssynch/synch.cs
@@ -43,6 +43,13 @@ namespace appsvc_fnc_dev_userssynch
             public string ReasonForRejection;
         }
 
+        struct SMTPInfo
+        {
+            public string SMTPPort { get; set; }
+            public string SMTPServerFQDN { get; set; }
+
+        }
+
         [FunctionName("synch")]
             public static async Task Run([TimerTrigger(" 0 */60 * * * *")] TimerInfo myTimer, ExecutionContext context, ILogger log)
         {
@@ -254,11 +261,33 @@ namespace appsvc_fnc_dev_userssynch
 
                             if (rejectedList.Count > 0)
                             {
+
+                                string SMTPInfoFileName = "general-config.json";
+                                BlobContainerClient blobContainerClientSMTP = new BlobContainerClient(new Uri(string.Format("https://{0}.blob.core.windows.net/{1}", accountName, "sync-bootstrap-config-v4")), new DefaultAzureCredential());
+                                var bcSMTP = blobContainerClientSMTP.GetBlobClient(SMTPInfoFileName);
+
+                                if (bcSMTP.Exists())
+                                {
+                                    using (var streamSMTP = await bcSMTP.OpenReadAsync())
+                                    using (var srSMTP = new StreamReader(streamSMTP))
+                                    using (var jrSMTP = new JsonTextReader(srSMTP))
+                                    {
+                                        var result = JsonSerializer.CreateDefault().Deserialize<SMTPInfo>(jrSMTP);
+                                        var smtpPort = result.SMTPPort;
+                                        var smtpServer = result.SMTPServerFQDN;
+
+                                        // SendRejectedList(string.Join(",", result.EmailNotificationListForUsersThatCannotBeInvited), rejectedList, log);
+                                    }
+                                }
+                                else
+                                {
+                                    log.LogError($"File {SMTPInfoFileName} not found");
+                                }
                                 // need to get email notification list here!
                                 // The list of email has to come from the storage account based on the department.
                                 // For example, TBS will be from: TBS-To-GCX-B2B-Sync.json, DFO from: DFO-To-GCX-B2B-Sync.json, etc.
 
-                                string notificationFileName = $"{group_alias}-To-GCX-B2B-Sync.json";
+                                string notificationFileName = $"{group_alias.ToUpper()}-To-GCX-B2B-Sync.json";
                                 BlobContainerClient blobContainerClient = new BlobContainerClient(new Uri(string.Format("https://{0}.blob.core.windows.net/{1}", accountName, "b2b-sync-config")), new DefaultAzureCredential());
                                 var bc = blobContainerClient.GetBlobClient(notificationFileName);
 
