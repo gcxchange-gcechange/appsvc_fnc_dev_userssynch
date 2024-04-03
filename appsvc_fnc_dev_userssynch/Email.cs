97121b29b03a0d536a721346fba233f7c2ecbd7c
oliver-tbs <oliver.postlethwaite@tbs-sct.gc.ca>
2024-04-03 15:59:13 -0400

    Code clean up after review

--- appsvc_fnc_dev_userssynch/Email.cs
+++ appsvc_fnc_dev_userssynch/Email.cs
@@ -31,8 +31,8 @@ namespace appsvc_fnc_dev_userssynch
                 };
 
                 var client = new SecretClient(new System.Uri(config["keyVaultUrl"]), new DefaultAzureCredential(), options);
-                KeyVaultSecret secret_password = client.GetSecret(config["secretNamePassword"]);
-                KeyVaultSecret sender_Email = client.GetSecret(config["senderEmail"]);
+                KeyVaultSecret secret_password = client.GetSecret(config["secretNamePassword"]);    // dgcx-dev-key-email-password
+                KeyVaultSecret sender_Email = client.GetSecret(config["senderEmail"]);              // dgcx-dev-key-email-sender
 
                 var senderPassword = secret_password.Value;
                 var senderEmail = sender_Email.Value;
@@ -61,39 +61,8 @@ namespace appsvc_fnc_dev_userssynch
             catch (Exception e) {
                 log.LogError($"Message: {e.Message}");
                 if (e.InnerException is not null) log.LogError($"InnerException: {e.InnerException.Message}");
+                log.LogError($"StackTrace: {e.StackTrace}");
             }
         }
-
-        //public static async void SendEmailGraph(string subject, string BodyContent, ILogger log)
-        //{
-        //    IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).AddEnvironmentVariables().Build();
-
-        //    string EmailSender = config["userId"];
-
-        //    var scopes = new[] { "user.read mail.send" };
-        //    ROPCConfidentialTokenCredential auth = new ROPCConfidentialTokenCredential(log);
-        //    GraphServiceClient graphClient = new GraphServiceClient(auth, scopes);
-
-        //    var msg = new Message
-        //    {
-        //        Subject = subject,
-        //        Body = new ItemBody
-        //        {
-        //            ContentType = BodyType.Html,
-        //            Content = BodyContent
-        //        },
-        //        ToRecipients = new List<Recipient>() { new Recipient { EmailAddress = new EmailAddress { Address = "oliver.postlethwaite@tbs-sct.gc.ca" } } }
-        //    };
-
-        //    try
-        //    {
-        //        await graphClient.Users[EmailSender].SendMail(msg).Request().PostAsync();
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        log.LogError($"Message: {e.Message}");
-        //        if (e.InnerException is not null) log.LogError($"InnerException: {e.InnerException.Message}");
-        //    }
-        //}
     }
 }
